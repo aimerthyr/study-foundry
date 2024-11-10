34242e6be39c4c6bd17512dce749abc0fb96b8d8
aimerthyr <aimerthy@foxmail.com>
2024-11-10 10:37:44 +0800

    feat: 自动格式化合约

--- src/ThirdParty.sol
+++ src/ThirdParty.sol
@@ -4,9 +4,12 @@ pragma solidity ^0.8.13;
 import "solmate/tokens/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-/** 通过 forge install 安装的第三方包， 直接通过 forge remappings > remappings.txt 来生成映射 */
-contract InstallThirdParty is ERC20("ForgeInstallToken", "FIT", 18)  {
-}
+/**
+ * 通过 forge install 安装的第三方包， 直接通过 forge remappings > remappings.txt 来生成映射
+ */
+contract InstallThirdParty is ERC20("ForgeInstallToken", "FIT", 18) {}
 
-/** 通过 npm install 安装的第三方包，需要手动到 remappings.txt 中添加映射（一般不常用） */
+/**
+ * 通过 npm install 安装的第三方包，需要手动到 remappings.txt 中添加映射（一般不常用）
+ */
 contract NpmThirdParty is ERC721("ForgeNpmToken", "FNT") {}
--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -12,19 +12,25 @@ import {Counter} from "../src/Counter.sol";
  */
 contract CounterTest is Test {
     Counter public counter;
-    
-    /** setUp 每次测试前都会被调用，必须是 public / external */
+
+    /**
+     * setUp 每次测试前都会被调用，必须是 public / external
+     */
     function setUp() public {
         counter = new Counter();
     }
 
-    /** 前缀 test 代表是一个测试函数 */
+    /**
+     * 前缀 test 代表是一个测试函数
+     */
     function testInc() public {
         counter.inc();
         assertEq(counter.get(), 1);
     }
 
-    /** 前缀 testFail 代表当前这个函数必须失败，才算通过，这种写法一般不容易看出具体错误 */
+    /**
+     * 前缀 testFail 代表当前这个函数必须失败，才算通过，这种写法一般不容易看出具体错误
+     */
     function testFailPrecisionOverflow() public {
         counter.dec(); // 因为是无符号整数初始值是 0 ，减 1 的话就会溢出
     }
@@ -32,7 +38,7 @@ contract CounterTest is Test {
     function testDec() public {
         counter.inc();
         counter.inc();
-        counter.dec(); 
+        counter.dec();
         assertEq(counter.get(), 1);
     }
 }
